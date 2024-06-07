f33d8260c3b532b222e94b6e9e1be6b75b6d62eb
unknown <Sunnat0401>
2024-06-07 19:02:41 +0300

    add file

---
+++ src/Components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full relative app'> 
+        <div className='absolute bg-black/80 inset-0 blur-3xl'/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Styles/index.css'
+import App from './Components/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
